fba9acef00b65f1409f4bb1eebae9d028268429a
theahmadsharif <theahmadsharif@gmail.com>
2020-05-21 04:29:05 +0600

    Activity

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
+import * as moment from 'moment';
+
+
+interface Activity {
+  date: {
+    year: any,
+    month: any,
+    day: any,
+  },
+  list: any,
+  note: string
+}
+
+
 
 @Component({
   selector: 'app-activity',
@@ -6,10 +21,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activity.component.css']
 })
 export class ActivityComponent implements OnInit {
+  isCollapsed:boolean = true;
+  activityCollapse:boolean = true;
+  notification:any = null;
+
+
+  activity:Activity = {
+    date: {year: new Date().getFullYear(), month: new Date().getMonth() + 1, day: new Date().getDate()},
+    list: '',
+    note: '',
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addActivity() {
+
   }
 
 }
