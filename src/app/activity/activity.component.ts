494fabdfadff9fa9419d624e8fe8fb97ffa37333
theahmadsharif <theahmadsharif@gmail.com>
2020-05-24 04:15:05 +0600

    Activity

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import * as _ from 'lodash';
 import * as moment from 'moment';
 
@@ -9,11 +10,8 @@ interface Activity {
     month: any,
     day: any,
   },
-  list: any,
-  note: string
+  list: string[]
 }
-
-
 
 @Component({
   selector: 'app-activity',
@@ -21,24 +19,40 @@ interface Activity {
   styleUrls: ['./activity.component.css']
 })
 export class ActivityComponent implements OnInit {
-  isCollapsed:boolean = true;
-  activityCollapse:boolean = true;
+  activityCollapse:boolean = false;
   notification:any = null;
-
+  event:any = [];
 
   activity:Activity = {
     date: {year: new Date().getFullYear(), month: new Date().getMonth() + 1, day: new Date().getDate()},
-    list: '',
-    note: '',
+    list: []
   }
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { 
+  }
 
   ngOnInit(): void {
   }
 
-  addActivity() {
+  addActivity(activity) {
 
+      var datetime = new Date().getTime();
+      var d = datetime.toString(); 
+
+      this.firestore.collection('Activity').doc(d).set({
+  
+            _id: datetime,
+            activity_date: activity.date,
+            activity_list: this.event,
+            note: activity.note,
+
+
+      });
+  }
+  listActivity(object) {
+    this.event.push(object);
+    this.activity.list = [];
+    console.log(this.event);
   }
 
 }
